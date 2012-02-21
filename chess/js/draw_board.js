41b7146a18564658a2627a5bdf02548572e50f1e
Yoan Durand <yoan.durand@free.fr>
2012-02-22 00:09:00 +0100

    draw a part of the chessboard

--- chess/js/draw_board.js
+++ chess/js/draw_board.js
@@ -21,23 +21,53 @@ window.onload = function ()
             alert ("impossible de recupere le context canvas");
             return;
         }
-        for (var i = 0; i < 8; i++)
+        /*
+        context.beginPath();
+        context.moveTo (0, 0);
+        context.lineTo(100,  0);
+        context.lineTo(100, 100);
+        context.lineTo(0,  100);                               
+        context.lineTo(0, 0);
+        context.fill ();
+        context.closePath();
+        context.beginPath();
+        context.lineTo(200,  0);
+        context.lineTo(200, 100);
+        context.lineTo(100,  100);                               
+        context.lineTo(100, 0);
+        context.stroke ();
+        context.closePath();
+        context.beginPath();
+        context.lineTo(300,  0);
+        context.lineTo(300, 100);
+        context.lineTo(200,  100);                               
+        context.lineTo(200, 0);
+        context.fill ();
+        context.closePath();*/
+
+        
+        
+        var i = 0;
+        var j = 0;
+        for (i = 0; i < 8; i++)
             {
-                for (var j = 0; j < 8; j++)
+                for (j = 0; j < 8; j++)
                     {            
                         if (i % 2 == 0)
                             {
                                 if (j % 2 == 0)    
                                 {
                                     context.beginPath();
-                                    context.moveTo(0, 0);
-                                    context.lineTo(0, 100);
-                                    context.lineTo(100, 100);
-                                    context.lineTo(100, 0);
-                                    context.lineTo(0, 0);
+                                    context.lineTo(((j+1)*100), (i*100));
+                                    context.lineTo(((j+1)*100) , ((i+1) * 100));
+                                    context.lineTo((j*100), (100*(i+1)));                               
+                                    context.lineTo((j * 100), (i*100));
                                     context.fill ();
+                                    context.closePath();
+                                    
                                 }
-                                else
+                            }
+                               /* else
                                     {
                                     context.beginPath();
                                     context.moveTo(0, 0);
@@ -70,7 +100,7 @@ window.onload = function ()
                                     context.lineTo(0, 0);
                                     context.fill();    
                                     }
-                                }
+                                }*/
                     }
             }
 }
