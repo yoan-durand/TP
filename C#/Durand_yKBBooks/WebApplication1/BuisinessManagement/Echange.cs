30cfad0a9f49724238d7c53ea42a23f5d0c4c8fa
yoan <durand.yoan@gmail.com>
2012-04-04 16:34:58 +0200

    finish all exercices and try to test

---
+++ C#/Durand_yKBBooks/WebApplication1/BuisinessManagement/Echange.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.BuisinessManagement
+{
+    public class Echange
+    {
+        public static List<DBO.Books> parse(string xmlname, int i)
+        {
+            DataAccess.Echange parse = new DataAccess.Echange();
+
+            if (i == 0)
+            {
+                return (parse.parseDom(xmlname));
+            }
+            else
+            {
+                return (parse.parse_linq (xmlname));
+            }
+        }
+
+        public static void create(List<DBO.Books> list, int i)
+        {
+            DataAccess.Echange echange = new DataAccess.Echange();
+
+            if (i == 0)
+            {
+                if (list.Count > 0)
+                {
+                    echange.create_DOM(list);
+                }
+            }
+            else
+            {
+                if (list.Count > 0)
+                {
+                    echange.create_linq (list);
+                }    
+            }
+        }
+    }
+}
