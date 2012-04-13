186039b060b58d64f3ff71563efd9b8053e9d2f4
yoan <durand.yoan@gmail.com>
2012-04-14 00:55:33 +0200

    Add the arborescence and all the needed classes

---
+++ NET/BUGTRACKER/durand_yBugTrack/durand_yBugTrack/durand_yBugTrack/BusinessManagement/Project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace durand_yBugTrack.BusinessManagement
+{
+    public class Project
+    {
+    }
+}
